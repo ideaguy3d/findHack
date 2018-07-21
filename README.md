51dc9fe8d5ecf860c9e5ffd68d8e6173e9a8d32a
Julius Alvarado <nbr1ninrsan2@gmail.com>
2018-07-21 11:25:25 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# StreetView App v2 UI
+
+[StreetView v2](https://maps.mhetadata.com/sv2)
+
+***
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.11.1.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/states/landing/ctrl.landing.js
@@ -0,0 +1,58 @@
+/**
+ * Created by Julius Alvarado on 9/4/2017.
+ */
+
+(function () {
+    "use strict";
+
+    angular.module('edhubJobsApp').controller('LandingCtrl', ['edhubJobPostService', '$location',
+        'smoothScroll', 'eOrgListFact', '$rootScope',
+        LandingClass
+    ]);
+
+    function LandingClass(edhubJobPostService, $location, smoothScroll, eOrgListFact, $rootScope) {
+
+        const vm = this;
+        vm.jobPostBg = "images/chalkboard3dArt1.png";
+        vm.showVid = true;
+
+        vm.apply2job = function (organizationName, postId) {
+            $location.url('/apply/' + postId + '/' + organizationName);
+        };
+
+        vm.apply2org = function (orgInfo) {
+            if($rootScope.rootEdhubAuthUser) {
+                $location.url('/apply/'+orgInfo.orgId+'/'+orgInfo.orgName);
+            } else {
+                $location.url('/signup/sta');
+            }
+
+        };
+
+        vm.scroll2recentJobs = function () {
+            var elem = document.getElementById("edhub-recent-jobs-landing-title");
+            smoothScroll(elem);
+        };
+
+        activate();
+
+        function activate() {
+            eOrgListFact.readFromOrgFeed(5, 'timestamp').$loaded().then(function (data) {
+                vm.orgFeed = data;
+            }).catch(function (error) {
+                console.error('edhub - Error: ', error);
+            });
+
+            /*
+            edhubJobPostService.jobPostingsLimitTo(7).$loaded().then(function (res) {
+                vm.jobPostings = res;
+                console.log("edhub - jobPostings res =");
+                console.log(res);
+            }).catch(function (error) {
+                console.error('edhub - Error: ', error);
+            });
+            */
+        }
+    }
+
+}());
